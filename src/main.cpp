6ea6d147cfb1cc22ee0d7cf6e19884ee4498f7bf
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2019-03-10 13:27:23 +0530

    corrected format

--- src/main.cpp
+++ src/main.cpp
@@ -18,58 +18,58 @@ using namespace cv::dnn;
 int main(int argc, char *argv[])
 {    
 
-	Mat image;
+    Mat image;
     image = imread(argv[1], IMREAD_COLOR);   // Read the file
-	
-	cout<< "image size"<<image.cols<<"x"<<image.rows<<endl;
-	
-	if(! image.data )                             
+
+    cout<< "image size"<<image.cols<<"x"<<image.rows<<endl;
+
+    if(! image.data )
     {
         cout <<  "Could not open or find the image" << std::endl ;
         return -1;
     }
-	
-   Mat inputBlob = blobFromImage(image, 1 , Size(image.cols, image.rows), Scalar(103.939, 116.779, 123.680), false, false); //Convert Mat to batch of images
-  
-   cout<<"loading net"<<endl;
-   
-   dnn::Net net;
-   net= readNetFromTorch(argv[2]);
-   vector< String > layers;
-   
+
+    Mat inputBlob = blobFromImage(image, 1 , Size(image.cols, image.rows), Scalar(103.939, 116.779, 123.680), false, false); //Convert Mat to batch of images
+
+    cout<<"loading net"<<endl;
+
+    dnn::Net net;
+    net= readNetFromTorch(argv[2]);
+    vector< String > layers;
+
     layers=net.getLayerNames ();
     for (std::vector<String>::iterator it = layers.begin() ; it != layers.end(); ++it)
     {
-		std::cout << "-> " << *it<<endl;
-	}
-	
-   if (net.empty())
+        std::cout << "-> " << *it<<endl;
+    }
+
+    if (net.empty())
     {
-     cout<<"Failed to load network"<<endl;
-     return -1;
-	}
-	
-   net.setInput(inputBlob);  
-   Mat styledMat = net.forward();
-   // convert tensor blob to  mat images
-   std::vector<cv::Mat> outImg;
-   imagesFromBlob(styledMat,outImg);
+        cout<<"Failed to load network"<<endl;
+        return -1;
+    }
+
+    net.setInput(inputBlob);
+    Mat styledMat = net.forward();
+    // convert tensor blob to  mat images
+    std::vector<cv::Mat> outImg;
+    imagesFromBlob(styledMat,outImg);
     
-	Mat styledImgOut;
-	
-	outImg[0].convertTo(styledImgOut,0);
-	
-	cout<<"Channels :"<<outImg[0].channels()<<endl;
+    Mat styledImgOut;
+
+    outImg[0].convertTo(styledImgOut,0);
+
+    cout<<"Channels :"<<outImg[0].channels()<<endl;
     cout<<"Depth :"<<styledImgOut.depth()<<endl;
-	cout<<"Res :"<<outImg[0].cols<<"x"<<outImg[0].rows<<endl;
-	
-	styledImgOut =styledImgOut+ Scalar(103.939, 116.779, 123.680);
+    cout<<"Res :"<<outImg[0].cols<<"x"<<outImg[0].rows<<endl;
 
-	namedWindow( "Style transfer", -1 );
-	imshow( "Style transfer", styledImgOut );      
-	waitKey(0);     
-	
+    styledImgOut =styledImgOut+ Scalar(103.939, 116.779, 123.680);
 
-   imwrite( "style_out.jpg", styledImgOut );
-  
+    namedWindow( "Style transfer", -1 );
+    imshow( "Style transfer", styledImgOut );
+    waitKey(0);
+
+
+    imwrite( "style_out.jpg", styledImgOut );
+
 }
